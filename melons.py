7fa08bc3015b0f9a00578c4b985517f7489c33d8
Hackbright Student <info@hackbrightacademy.com>
2016-01-15 13:05:59 -0800

    added further study to exercise

--- melons.py
+++ melons.py
@@ -1,6 +1,14 @@
 from random import randint
+import time
 
 """This file should have our order classes in it."""
+
+class TooManyMelonsError(ValueError):
+    """Raise an error if order qty exceeds 100."""
+
+    def __init__(self):
+        return super(TooManyMelonsError, self).__init__("No more than 100 melons, please!")
+
 class AbstractMelonOrder(object):
     """A melon order."""
 
@@ -8,24 +16,33 @@ class AbstractMelonOrder(object):
         """Initialize melon order attributes"""
 
         self.species = species
+        if qty > 100:
+            raise TooManyMelonsError
         self.qty = qty
         self.country_code = country_code
         self.shipped = False
-        # self.base_price = 5
-        # self.base_price = self.get_base_price()
         self.base_price = randint(5, 9)
 
     def get_base_price(self):
-        # base_price = randint(5, 9)
-        # return base_price
+        """Calculate the base price, considering whether surge pricing applies."""
+    
+        localtime_now = time.localtime(time.time())
+        localtime_now_hour = localtime_now.tm_hour
+        localtime_now_day = localtime_now.tm_wday
+
+        if (localtime_now_hour in range(8,14)) and (localtime_now_day in range(0,5)):
+            self.base_price += 4
+
         return self.base_price
 
     def get_total(self):
         """Calculate total order price."""
 
+        # print self.base_price
+
         base_price = self.get_base_price()
 
-        print base_price
+        # print "{0:.2f}".format(base_price)
 
         if self.species == "Christmas":
             base_price *= 1.5
@@ -35,7 +52,9 @@ class AbstractMelonOrder(object):
         # if self.qty < 10 and self.country_code != 'US':
         #     total = total + 3
 
-        return total
+        rounded_total = round(total, 2)
+
+        return rounded_total
 
     def mark_shipped(self):
         """Set initialized attribute shipped to true."""
@@ -45,6 +64,8 @@ class AbstractMelonOrder(object):
         """Return the country code."""
 
         return self.country_code
+
+
 
 
 
@@ -121,3 +142,5 @@ class GovernmentMelonOrder(AbstractMelonOrder):
         """Inspect US government order and update inspection status."""
 
         self.passed_inspection = passed
+
+    
